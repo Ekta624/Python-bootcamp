8a99f87be6f557f931c97a72af818aa05bf7b58e
Ekta624 <ekta74226@gmail.com>
2024-09-19 10:26:22 +0530

    done

---
+++ tupplesdemo.py
@@ -0,0 +1,35 @@
+# myTuple=("lokesh","astha","ekta","astha")
+# print(type(myTuple))
+# #myTuple.
+# print(myTuple)
+# print(myTuple[1])
+# for data in myTuple:
+#     print(data)
+#myDict={
+#     "name":"Ekta",
+#     "email":"ekta123@gmail.com",
+#     "mobile":"7412503225"
+# }
+#print(type(myDict))
+
+#print(myDict)
+#for item in myDict:
+ #   print(myDict[item])
+# class ekta:
+#    age=19
+#    print("i am from up")
+# ekta=ekta()
+# print(ekta.age)
+
+import datetime
+class AgeCalculator:
+    cage=int(input("enter your current year:"))
+    bage=(int(input("Enter your birthyear:")))
+    age=cage-bage
+    dob="3 november 2004"
+   
+myage=AgeCalculator()
+
+
+print("your age is:",myage.age,myage.dob)
+
